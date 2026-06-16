049b34f55f39e1003bf5237b8e61da2c310790b9
Donna Chen <donnaxtc@gmail.com>
2026-06-15 22:33:09 -0400

    drop and created tables

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -11,3 +11,37 @@ export async function createOrder(date, note, userId) {
   } = await db.query(sql, [date, note, userId]);
   return order;
 }
+
+export async function getOrdersByUserId(userId) {
+  const sql = `
+    SELECT *
+    FROM orders
+    WHERE user_id = $1
+  `;
+  const { rows: orders } = await db.query(sql, [userId]);
+  return orders;
+}
+
+export async function getOrderById(id) {
+  const sql = `
+    SELECT *
+    FROM orders
+    WHERE id = $1
+  `;
+  const {
+    rows: [order],
+  } = await db.query(sql, [id]);
+  return order;
+}
+
+/** Orders made by the given user that include the given product */
+export async function getOrdersByUserIdAndProductId(userId, productId) {
+  const sql = `
+    SELECT orders.*
+    FROM orders
+    JOIN orders_products AS op ON op.order_id = orders.id
+    WHERE orders.user_id = $1 AND op.product_id = $2
+  `;
+  const { rows: orders } = await db.query(sql, [userId, productId]);
+  return orders;
+}
